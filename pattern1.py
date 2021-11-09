3d907254715982161373c81a5e1634f11d1761c4
test67a <test908127@gmail.com>
2021-11-09 13:38:56 +0530

    intial commit

---
+++ pattern1.py
@@ -0,0 +1,5 @@
+for i in range(5):
+    print()
+    for j in range(5):
+        print('*',end='')
+print('hello world...')
---
+++ pattern3.py
@@ -0,0 +1,6 @@
+for i in range(5):
+    print()
+    for k in range(5-i-1):
+        print(' ',end='')
+    for j in range(i+1):
+        print('*',end='')
